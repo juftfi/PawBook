ac78a3bf51e707ed43823b7a25624aed91e1da1b
juftfi <fourswapxyz@gmail.com>
2026-04-12 20:45:31 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="hidden md:flex items-center gap-4 text-sm">
-          <Link to="https://flaunch.gg/base/coins/0xB78B0dd87d4F660f6C455F65F447BD707080F752" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
+          <Link to="https://four.meme/" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
             $PAW
           </Link>
           <Link to="/communities" className="text-white hover:text-white/80 transition-colors font-bold">
