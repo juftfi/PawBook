413f700c77c60a051bbb44d73b0cce30c6eb5ac3
juftfi <fourswapxyz@gmail.com>
2026-04-13 11:36:41 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="hidden md:flex items-center gap-4 text-sm">
-          <Link to="https://pump.fun/" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
+          <Link to="https://pump.fun/coin/74WayeyoktvNK7YkmwM5q4CW2QsYkwUyUoafuWzHpump" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
             $PAW
           </Link>
           <Link to="/communities" className="text-white hover:text-white/80 transition-colors font-bold">
