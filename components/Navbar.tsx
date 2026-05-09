a8130eeabdbcc79d97a2d257229a71b640bc8e73
juftfi <107974770+juftfi@users.noreply.github.com>
2026-05-10 01:20:17 +0700

    1

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar: React.FC = () => {
         </div>
 
         <div className="hidden md:flex items-center gap-4 text-sm">
-          <Link to="https://four.meme/en/token/0x90ab300c8b68b0eff7dc6bf9adb7650d24c14444" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
+          <Link to="https://pump.fun/" target="_blank" className="text-white hover:text-white/80 transition-colors font-bold">
             $PAW
           </Link>
           <Link to="/communities" className="text-white hover:text-white/80 transition-colors font-bold">
